4e1f163e0db7ba7d966ccd6264ac5c292b8194d5
Johannes Maier <johannes.maier@active-group.de>
2023-05-08 16:13:57 +0200

    update

--- vorkurs-johannes/rev.rkt
+++ vorkurs-johannes/rev.rkt
@@ -55,7 +55,7 @@
       ((empty? ls) acc) ; 4 3 2 1
       ((cons? ls)       ; ls ist (list 4), acc ist (list 3 2 1)
        (rev* (rest ls)
-             (cons (first list) acc))))))
+             (cons (first ls) acc))))))
 
 
 
