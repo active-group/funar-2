7702904850f56e5761515a6ddc522135e75f8993
Johannes Maier <johannes.maier@active-group.de>
2023-05-08 09:46:06 +0200

    update

--- vorkurs-johannes/image.rkt
+++ vorkurs-johannes/image.rkt
@@ -1,2 +1,5 @@
-#lang deinprogramm/sdp/beginner
+;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
+;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
+#reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname image) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
+;;#lang deinprogramm/sdp/beginner
 
