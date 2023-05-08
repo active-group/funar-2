e0c2e5cecc727c9766a547aed7af9f5828e880f7
Johannes Maier <johannes.maier@active-group.de>
2023-05-08 15:18:32 +0200

    update

--- vorkurs-johannes/animal.rkt
+++ vorkurs-johannes/animal.rkt
@@ -247,7 +247,7 @@ FP: neue Fälle schwer, neue Operationen einfach
 (define-record (cons-list-of element) ; Makro generiert lambdas für uns
   cons
   cons?
-  (first number)
+  (first element)
   (rest (list-of element)))
 
 (define list1 (cons 5 empty))
