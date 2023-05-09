9877a53d22526254fc2f751fde800256c68ec880
Johannes Maier <johannes.maier@active-group.de>
2023-05-09 11:36:21 +0200

    update

--- haskell-code/Intro.hs
+++ haskell-code/Intro.hs
@@ -162,3 +162,5 @@ tupelFun t = fst t + snd t + 3
 --          v  Typvariable (%a in Racket)
 tuplify :: (a -> b -> c) -> ((a, b) -> c)
 tuplify    f              = \(a, b) -> (f a) b
+
+-- (tuplify (+)) (2, 3)
