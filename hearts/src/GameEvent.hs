12e608b178cbfa7d2dcb71a19567a52e130a094d
Johannes Maier <johannes.maier@active-group.de>
2023-05-10 16:23:24 +0200

    update

--- hearts/src/GameEvent.hs
+++ hearts/src/GameEvent.hs
@@ -24,16 +24,26 @@ import Data.Map (Map)
 -- Bitte, dass etwas passieren soll _in der Zukunft_
 
 -- Was passiert bei Hearts?
-data GameEvent
-    = GameEnded Player -- Gewinner
-    | GameStarted Player -- optional, weil Kreuz 2 beginnt
-    -- | CardsShuffled
-    | CardsDistributed Player Hand
-    | CardPlayed Player Card
-    | TrickTaken Player Trick
-    | PlayerTurnChanged Player
-    | IllegalCardAttempted Player Card
+-- data GameEvent
+--     = GameEnded Player -- Gewinner
+--     | GameStarted Player -- optional, weil Kreuz 2 beginnt
+--     -- | CardsShuffled
+--     | CardsDistributed Player Hand
+--     | CardPlayed Player Card
+--     | TrickTaken Player Trick
+--     | PlayerTurnChanged Player
+--     | IllegalCardAttempted Player Card
 
 data GameCommand
     = PlayCard Player Card
     | DealHands (Map Player Hand)
+
+data GameEvent
+    = HandDealt Player Hand
+    | PlayerTurnChanged Player
+    | LegalCardPlayed Player Card
+    | TrickTaken Player Trick
+    -- könnte auch zweiteilen in TrickClosed und TrickTaken
+    | GameEnded Player
+    | IllegalCardAttempted Player Card
+    deriving Show
