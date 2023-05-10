df5ee4ab9060874f39e8726bfe68fda8c6870452
Johannes Maier <johannes.maier@active-group.de>
2023-05-10 10:46:21 +0200

    update

--- haskell-code/DB.hs
+++ haskell-code/DB.hs
@@ -6,11 +6,11 @@ Wollen: Key-Value-Store verwenden
 
 Würde gerne schreiben:
 
-
   put "Johannes" 36
   x = get "Johannes"
   put "Johannes" (x + 1)
   y = get "Johannes"
   return (show (x + y))
+
 
 -}
