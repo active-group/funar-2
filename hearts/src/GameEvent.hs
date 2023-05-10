fee93719c3b14988fa0a0f72948d43169162814c
Johannes Maier <johannes.maier@active-group.de>
2023-05-10 16:37:42 +0200

    update

--- hearts/src/GameEvent.hs
+++ hearts/src/GameEvent.hs
@@ -59,7 +59,7 @@ instance Monad Game where
     (>>=) (Done a) next = next a
     return = Done
 
-isCardValidM :: Game Bool
+isCardValidM :: Player -> Card -> Game Bool
 isCardValidM = undefined
 
 -- Ergebnis: der Ablauf, der passiert, wenn das Command behandelt wird
