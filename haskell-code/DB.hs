32aa80f595ac083194984be1d3e25f3363a2aa0d
Johannes Maier <johannes.maier@active-group.de>
2023-05-10 10:45:50 +0200

    update

--- haskell-code/DB.hs
+++ haskell-code/DB.hs
@@ -4,4 +4,13 @@ module DB where
 
 Wollen: Key-Value-Store verwenden
 
+Würde gerne schreiben:
+
+
+  put "Johannes" 36
+  x = get "Johannes"
+  put "Johannes" (x + 1)
+  y = get "Johannes"
+  return (show (x + y))
+
 -}
