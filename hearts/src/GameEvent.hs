5a73b6e649b1fd7dee9d5ff367a90268b3532add
Johannes Maier <johannes.maier@active-group.de>
2023-05-10 16:14:47 +0200

    update

--- hearts/src/GameEvent.hs
+++ hearts/src/GameEvent.hs
@@ -4,3 +4,31 @@ import Cards
 
 import qualified Data.Map as Map
 import Data.Map (Map)
+
+-- Events: Log von Dingen, die in der Anwendung passieren
+-- vollständig! enthält alles, was passiert ist
+
+-- -> Erzählung
+
+-- (kein) Event-Sourcing: Events als Quelle der Wahrheit
+-- ES ist Persistenz
+
+-- Events:
+-- - liegen in der Vergangenheit
+-- - enthält keinen Zustand
+-- - vollständig
+-- - fachlich motiviert / orientiert
+-- - Redundanz ist OK
+
+-- Commands:
+-- Bitte, dass etwas passieren soll _in der Zukunft_
+
+-- Was passiert bei Hearts?
+data GameEvent
+    = GameEnded Player -- Gewinner
+    | GameStarted Player
+    | CardsShuffled
+    | CardsDistributed Player Hand
+    | CardPlayed Player Card
+    | TrickTaken Player Trick
+    | PlayerTurnChanged Player
