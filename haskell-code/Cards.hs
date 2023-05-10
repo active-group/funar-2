d0df0e5bcba6a92ead970e39443bb0d4988af3d5
Johannes Maier <johannes.maier@active-group.de>
2023-05-10 15:40:11 +0200

    update

--- haskell-code/Cards.hs
+++ haskell-code/Cards.hs
@@ -1,2 +1,10 @@
 module Cards where
 
+-- Modell für das franz. Blatt
+
+-- Eine Spielkarte besteht aus:
+-- - Farbe (Suit)
+-- - Wert (Rank)
+
+data Suit = Clubs | Spades | Hearts | Diamonds
+    deriving (Eq, Show)
