75f2c1f000d3cc9440c2254b186c55a2dfce3881
Johannes Maier <johannes.maier@active-group.de>
2023-05-10 10:01:34 +0200

    update

--- haskell-code/Intro.hs
+++ haskell-code/Intro.hs
@@ -327,7 +327,7 @@ instance Semigroup [a] where
 -- funktioniert schon für String: :info String!
 
 -- Monoid als "Interface" "erbt" von Semigroup
-class Semigroup t => Monoid t where
+class MySemigroup t => MyMonoid t where
     -- Gesetz:   neutral `op` x == x == x `op` neutral
     neutral :: t
 
