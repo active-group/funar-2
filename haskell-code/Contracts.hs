2a7e1f18d3aeea8f7e77ae1ba94a91f220c21ce3
Johannes Maier <johannes.maier@active-group.de>
2023-05-11 13:59:58 +0200

    update

--- haskell-code/Contracts.hs
+++ haskell-code/Contracts.hs
@@ -3,6 +3,11 @@ module Contracts where
 -- Zero-Coupon-Bond, einfaches Beispiel für Vertrag
 -- "Ich bekomme am 24.12.2023 100 Euro."
 
+-- Currency-Swap
+--  Am 24.12.2023 passiert Folgendes:
+--  Ich bekomme 100 EUR
+--  Ich bezahl 150 GBP
+
 data Date = MkDate String
   deriving (Eq, Show, Ord)
 
@@ -14,3 +19,6 @@ data Currency = EUR | GBP | USD | YEN
 data Contract =
     ZeroCouponBond Date Amount Currency
     deriving (Show)
+
+zcb :: Contract
+zcb = ZeroCouponBond (MkDate "24.12.2023") 100 EUR
